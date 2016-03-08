e69bde133d2aef0472f19653d87c3b45d0e46849
mattkemball <matthewkemball@Matthews-MacBook-Pro.local>
2016-03-08 17:32:47 -0500

    html arranger
    fixed vars

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 var $belowH1 = $('<h1>');                  
 $belowH1.html('Below ground veggies.'); 
 $('body').prepend($belowH1);
-
 var $belowVeggies = $('<ul>');                  
 $('body').append($belowVeggies);
-
 var $aboveH1 = $('<h1>');                  
 $aboveH1.html('Above ground veggies.'); 
 $('body').append($aboveH1);
-
 var $aboveVeggies = $('<ul>');                  
 $('body').append($aboveVeggies);
 
